b42036e45513f3ff7da352b6617797f2855c6ce1
DilaraGlr <guler.dilara2000@gmail.com>
2025-11-14 10:47:50 +0100

    ex02: OK

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,34 @@
+def all_thing_is_obj(object: any) -> int:
+    if type(object).__name__ == "list":
+        print(f"List : {type(object)}")
+    elif type(object).__name__ == "tuple":
+        print(f"Tuple : {type(object)}")
+    elif type(object).__name__ == "set":
+        print(f"Set : {type(object)}")
+    elif type(object).__name__ == "dict":
+        print(f"Dict : {type(object)}")
+    elif type(object).__name__ == "str":
+        if object == "Brian":
+            print(f"Brian is in the kitchen : {type(object)}")
+        elif object == "Toto":
+            print(f"Toto is in the kitchen : {type(object)}")
+        else:
+            print(f"str: {type(object)}")
+    else:
+        print("Type not found")
+
+    return 42
+
+
+# from find_ft_type import all_thing_is_obj
+# ft_list = ["Hello", "tata!"]
+# ft_tuple = ("Hello", "toto!")
+# ft_set = {"Hello", "tutu!"}
+# ft_dict = {"Hello" : "titi!"}
+# all_thing_is_obj(ft_list)
+# all_thing_is_obj(ft_tuple)
+# all_thing_is_obj(ft_set)
+# all_thing_is_obj(ft_dict)
+# all_thing_is_obj("Brian")
+# all_thing_is_obj("Toto")
+# print(all_thing_is_obj(10))
